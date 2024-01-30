d03e0bda088ea83ed0cfe5de7671ed8d1fd5b4bc
tunghq <tunghq.861@gmail.com>
2024-01-30 18:35:23 +0700

    change db

--- Database/QL_Ban_Ve_Xem_Phim.sql
+++ Database/QL_Ban_Ve_Xem_Phim.sql
@@ -85,24 +85,24 @@ VALUES				('ducanh',    	'ducanh0202@gmail.com', 	'$2a$10$W2neF9.6Agi6kAKVq8q3fe
                     ('nguyenvanb', 	'nguyenb@gmail.com', 		'$2a$10$W2neF9.6Agi6kAKVq8q3fec5dHW8KUA.b0VSIGdIZyUravfLpyIFi', 'Nguyen', 	'B', 		'User', 1);
                      
 INSERT INTO `Film` (`name`, `directors`, `actors`, `genre`, `duration`, `description`, `release_date`, `ticket_price`, `poster`, `creator_id`)
-VALUES				('Năm đêm kinh hoàng', 'Emma Tammi','Matthew Lillard, Josh Hutcherson, Mary Stuart Masterson', 'Kinh Dị', '110 phút', 'Nhân viên bảo vệ Mike bắt đầu làm việc tại Freddy Fazbear pizza...', '2024/10/25', 70000,'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png' , 1),
-					('Kị sĩ bóng đêm', 'Christopher Nolan', 'Christian Bale, Michael Caine, Heath Ledger, Aaron Eckhart', 'Hành động', '152 phút', 'Kị sĩ bóng đêm mở đầu bằng cuộc oanh tạc...', '2024/10/18', 70000,'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png', 1),
-                    ('Wolfoo và hòn đảo kỳ bí', 'Phan Thị Thơ', 'Sony Minh Hiếu, Đạt Phi, Như Ý', 'Hài, Hoạt Hình', '100 phút', 'Câu chuyện xoay quanh nhân vật chính là chú sói nhỏ ...', '2024/10/13', 70000,'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png', 2),
-                    ('Vầng trăng máu', 'Martin Scorsese', 'Leonardo DiCaprio, Robert De Niro, Lily Gladstone', 'Bí ẩn, Hồi hộp', '206 phút', 'Vào những năm 1920, các thành viên của khu tự trị Osage...', '2024/10/20', 70000,'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png', 3),
-                    ('Cô giáo em là số 1', 'Park Jin - pyo', 'Shin Hae-sun, Lee Jun-young, Cha Chung-hwa,...', 'Hài hước, Hành động', '112 phút', 'Si-min là một võ sĩ quyền anh đầy triển vọng nhưng đã từ bỏ quyền thi đấu tại kỳ Thế vận hội Olympic để lấy tiền trả nợ cho cha.', '2024/11/24', 70000, 'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png', 1),
-                    ('Biệt đội Marvel', 'Nia DaCosta', 'Brie Larson, Iman Vellani, Teyonah Parris, Zawe Ashton', 'Hành động, Phiêu lưu', '104 phút', 'Carol Danvers bị vướng vào sức mạnh của Kamala Khan và Monica Rambeau, buộc họ phải hợp tác cùng nhau để cứu vũ trụ.', '2024/11/10', 70000, 'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png', 1),
-                    ('Quỷ Lùn Tinh Nghịch: Đồng Tâm Hiệp Nhạc', 'Tim Heitz', 'Anna Kendrick, Justin Timberlake, Troye Sivan', 'Phiêu lưu, Âm Nhạc', '92 phút', 'Sự xuất hiện đột ngột của John Dory, anh trai thất lạc đã lâu của Branch mở ra quá khứ bí mật được che giấu bấy lâu nay của Branch.', '2024/11/17', 70000, 'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png', 1),
-                    ('Đấu Trường Sinh Tử: Khúc Hát Của Chim Ca Và Rắn Độc', 'Francis Lawrence', 'Rachel Zegler, Hunter Schafer, Tom Blyth, Jason Schwartzman', 'Hành động, Phiêu lưu', '157 phút', 'Đấu Trường Sinh Tử: Khúc Hát Của Chim Ca Và Rắn Độc là phần tiền truyện của mạch truyện chính, lấy bối cảnh 64 năm trước phần phim đầu tiên.', '2024/11/17', 100000, 'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png',1);
+VALUES				('Năm đêm kinh hoàng', 'Emma Tammi','Matthew Lillard, Josh Hutcherson, Mary Stuart Masterson', 'Kinh Dị', '110 phút', 'Nhân viên bảo vệ Mike bắt đầu làm việc tại Freddy Fazbear pizza...', '2024/01/30', 70000,'https://files.betacorp.vn/files/media%2fimages%2f2023%2f10%2f03%2f700x1000-5demkinhhoang-115804-031023-17.png' , 1),
+					('Kị sĩ bóng đêm', 'Christopher Nolan', 'Christian Bale, Michael Caine, Heath Ledger, Aaron Eckhart', 'Hành động', '152 phút', 'Kị sĩ bóng đêm mở đầu bằng cuộc oanh tạc...', '2024/10/18', 70000,'https://upload.wikimedia.org/wikipedia/vi/2/2d/Poster_phim_K%E1%BB%B5_s%C4%A9_b%C3%B3ng_%C4%91%C3%AAm_2008.jpg', 1),
+                    ('Wolfoo và hòn đảo kỳ bí', 'Phan Thị Thơ', 'Sony Minh Hiếu, Đạt Phi, Như Ý', 'Hài, Hoạt Hình', '100 phút', 'Câu chuyện xoay quanh nhân vật chính là chú sói nhỏ ...', '2024/10/13', 70000,'https://lh3.googleusercontent.com/fPt0RQveClt8enn7d__GuA6wVjx11DxnynGh92gaiGbNIulSrtVKRwWfUbNLlHaQJv6D3X8Lne4jfyEcqTxOf1LqjOGs7LPLcOh9xWh0qg=w622', 2),
+                    ('Vầng trăng máu', 'Martin Scorsese', 'Leonardo DiCaprio, Robert De Niro, Lily Gladstone', 'Bí ẩn, Hồi hộp', '206 phút', 'Vào những năm 1920, các thành viên của khu tự trị Osage...', '2024/10/20', 70000,'https://upload.wikimedia.org/wikipedia/vi/thumb/f/fc/V%E1%BA%A7ng_tr%C4%83ng_m%C3%A1u_-_Official_Vietnam_poster.jpg/220px-V%E1%BA%A7ng_tr%C4%83ng_m%C3%A1u_-_Official_Vietnam_poster.jpg', 3),
+                    ('Cô giáo em là số 1', 'Park Jin - pyo', 'Shin Hae-sun, Lee Jun-young, Cha Chung-hwa,...', 'Hài hước, Hành động', '112 phút', 'Si-min là một võ sĩ quyền anh đầy triển vọng nhưng đã từ bỏ quyền thi đấu tại kỳ Thế vận hội Olympic để lấy tiền trả nợ cho cha.', '2024/11/24', 70000, 'https://www.cgv.vn/media/catalog/product/cache/1/image/1800x/71252117777b696995f01934522c402d/r/s/rsz_aw-ctkc-700x1000.jpg', 1),
+                    ('Biệt đội Marvel', 'Nia DaCosta', 'Brie Larson, Iman Vellani, Teyonah Parris, Zawe Ashton', 'Hành động, Phiêu lưu', '104 phút', 'Carol Danvers bị vướng vào sức mạnh của Kamala Khan và Monica Rambeau, buộc họ phải hợp tác cùng nhau để cứu vũ trụ.', '2024/11/10', 70000, 'https://photo-cms-anninhthudo.epicdn.me/w800/Uploaded/2024/xtsmr/2023_11_11/biet-doi-marvel-4583-8523.jpg', 1),
+                    ('Quỷ Lùn Tinh Nghịch: Đồng Tâm Hiệp Nhạc', 'Tim Heitz', 'Anna Kendrick, Justin Timberlake, Troye Sivan', 'Phiêu lưu, Âm Nhạc', '92 phút', 'Sự xuất hiện đột ngột của John Dory, anh trai thất lạc đã lâu của Branch mở ra quá khứ bí mật được che giấu bấy lâu nay của Branch.', '2024/11/17', 70000, 'https://upload.wikimedia.org/wikipedia/vi/thumb/3/3a/TROLLS_3_Vietnam_poster.jpg/220px-TROLLS_3_Vietnam_poster.jpg', 1),
+                    ('Đấu Trường Sinh Tử: Khúc Hát Của Chim Ca Và Rắn Độc', 'Francis Lawrence', 'Rachel Zegler, Hunter Schafer, Tom Blyth, Jason Schwartzman', 'Hành động, Phiêu lưu', '157 phút', 'Đấu Trường Sinh Tử: Khúc Hát Của Chim Ca Và Rắn Độc là phần tiền truyện của mạch truyện chính, lấy bối cảnh 64 năm trước phần phim đầu tiên.', '2024/11/17', 100000, 'https://www.cgv.vn/media/catalog/product/cache/1/image/c5f0a1eff4c394a251036189ccddaacd/p/o/poster_payoff_1_.jpg',1);
                 
 INSERT INTO `Film_Schedule` (`film_id`, `seat_number`, `time_slot`)
-VALUES							(1, 100, '2024/10/25'),
-								(2, 50, '2024/10/26'),
-								(2, 50, '2024/10/27'),
-								(2, 70, '2024/10/28'),
-								(2, 100, '2024/10/29'),
-                                (3, 70, '2024/10/27'),
-                                (3, 100, '2024/10/28'),
-                                (4, 100, '2024/10/29');
+VALUES							(1, 100, '2024/01/30'),
+								(2, 50, '2024/01/31'),
+								(3, 50, '2024/02/01'),
+								(4, 70, '2024/02/03'),
+								(2, 100, '2024/02/04'),
+                                (3, 70, '2024/02/05'),
+                                (6, 100, '2024/02/02'),
+                                (4, 100, '2024/01/30');
                                 
 INSERT INTO `Ticket` (`creator_id`, `schedule_id`, `quantity`, `total`, `booking_date`, `status`)
 VALUES				(8, 1, 2, 140000, '2024/10/30', 1),
